f6439d6aa6f5c7627193146b2eda9888b8e2320e
Klemp <klempd@8c8590432935.ant.amazon.com>
2019-03-27 14:29:31 -0700

    new layout for project 03

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
 'use strict';
 
-var myQuestions = [
-  ['Am I handsome? Yes/No'],
-  ['Am I awesome? Yes/No'],
-  ['Do I sit in back 2 rows? Yes/No'],
-  ['Do I like you? Yes/No'],
-  ['Was I already an Amazonian? Yes/No']
-];
-var myAnswers = [
-  ['YES'],
-  ['YES'],
-  ['YES'],
-  ['YES'],
-  ['YES']
-];
+// var myQuestions = [
+//   ['Am I handsome? Yes/No'],
+//   ['Am I awesome? Yes/No'],
+//   ['Do I sit in back 2 rows? Yes/No'],
+//   ['Do I like you? Yes/No'],
+//   ['Was I already an Amazonian? Yes/No']
+// ];
+// var myAnswers = [
+//   ['YES'],
+//   ['YES'],
+//   ['YES'],
+//   ['YES'],
+//   ['YES']
+// ];
 
-// // personalize user experience with their name
-var userName = prompt('What is your name?');
-if(userName === 'doug'){
-  console.log('Hey, we\'ve got the same name!');
-  alert('Hey, we\'ve got the same name!');
-} else {
-  console.log('Hi, ' + userName + '.  Welcome to the site!');
-  alert('Hi, ' + userName + '.  Welcome to the site!');
-}
+// // // personalize user experience with their name
+// var userName = prompt('What is your name?');
+// if(userName === 'doug'){
+//   console.log('Hey, we\'ve got the same name!');
+//   alert('Hey, we\'ve got the same name!');
+// } else {
+//   console.log('Hi, ' + userName + '.  Welcome to the site!');
+//   alert('Hi, ' + userName + '.  Welcome to the site!');
+// }
 
-for(var i=0; i<myQuestions.length; i += 1 ) { // loop through questions
-  var userAnswer = prompt(myQuestions[i][0]); // get user input
-  for(var j=0; j<myQuestions[i].length; j += 1 ) { // inner loop to check user input vs my answers
-    if(userAnswer.toUpperCase() === myAnswers[j][0]){
-      alert('You\'re Right!');
-    } else {
-      alert('Nope, that\'s wrong...');
-    }
-  }
-}
+// for(var i=0; i<myQuestions.length; i += 1 ) { // loop through questions
+//   var userAnswer = prompt(myQuestions[i][0]); // get user input
+//   for(var j=0; j<myQuestions[i].length; j += 1 ) { // inner loop to check user input vs my answers
+//     if(userAnswer.toUpperCase() === myAnswers[j][0]){
+//       alert('You\'re Right!');
+//     } else {
+//       alert('Nope, that\'s wrong...');
+//     }
+//   }
+// }
 
 // guess the final answer
 var finalAnswer = prompt('So what\'s my name?');
@@ -44,8 +44,8 @@ if(finalAnswer === 'doug'){
   document.getElementById('picbox').classList.add('win');
   document.getElementById('result').innerHTML = 'Great job mate!!!';
 } else {
-  console.log(userName + ', that\'s not my name, sorry.');
-  alert('Hi, ' + userName + ', that\'s not my name, sorry.');
+  console.log('That\'s not my name, sorry.');
+  alert('That\'s not my name, sorry.');
   document.getElementById('picbox').classList.add('fail');
   document.getElementById('result').innerHTML = 'Sorry, that\'s not me, Try again...';
 }
