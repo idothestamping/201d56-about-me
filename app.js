d7a27be4698031ef61ab0f28d86965662715a929
idothestamping <douglas.klemp@gmail.com>
2019-03-27 14:56:22 -0700

    refactored questions script to reciece yes/no answers

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 'use strict';
 
-// var myQuestions = [
-//   ['Am I handsome? Yes/No'],
-//   ['Am I awesome? Yes/No'],
-//   ['Do I sit in back 2 rows? Yes/No'],
-//   ['Do I like you? Yes/No'],
-//   ['Was I already an Amazonian? Yes/No']
-// ];
-// var myAnswers = [
-//   ['YES'],
-//   ['YES'],
-//   ['YES'],
-//   ['YES'],
-//   ['YES']
-// ];
+var myQuestions = [
+  ['Am I handsome? Yes/No'],
+  ['Am I awesome? Yes/No'],
+  ['Did I make this site in 1hr? Yes/No'],
+  ['Do I like Trump? Yes/No'],
+  ['Was I already an Amazonian? Yes/No']
+];
+var myAnswers = [
+  ['YES'],
+  ['YES'],
+  ['NO'],
+  ['NO'],
+  ['YES']
+];
 
-// // // personalize user experience with their name
-// var userName = prompt('What is your name?');
-// if(userName === 'doug'){
-//   console.log('Hey, we\'ve got the same name!');
-//   alert('Hey, we\'ve got the same name!');
-// } else {
-//   console.log('Hi, ' + userName + '.  Welcome to the site!');
-//   alert('Hi, ' + userName + '.  Welcome to the site!');
-// }
+// // personalize user experience with their name
+var userName = prompt('What is your name?');
+if(userName === 'doug'){
+  console.log('Hey, we\'ve got the same name!');
+  alert('Hey, we\'ve got the same name!');
+} else {
+  console.log('Hi, ' + userName + '.  Welcome to the site!');
+  alert('Hi, ' + userName + '.  Welcome to the site!');
+}
 
 // for(var i=0; i<myQuestions.length; i += 1 ) { // loop through questions
 //   var userAnswer = prompt(myQuestions[i][0]); // get user input
-//   for(var j=0; j<myQuestions[i].length; j += 1 ) { // inner loop to check user input vs my answers
+//   for(var j=0; j<myAnswers[i].length; j += 1 ) { // inner loop to check user input vs my answers
 //     if(userAnswer.toUpperCase() === myAnswers[j][0]){
 //       alert('You\'re Right!');
 //     } else {
@@ -35,6 +35,15 @@
 //     }
 //   }
 // }
+
+for(var i=0; i<myQuestions.length; i += 1 ) { // loop through questions
+  var userAnswer = prompt(myQuestions[i][0]); // get user input
+    if(userAnswer.toUpperCase() === myAnswers[i][0]){
+      alert('You\'re Right!');
+    } else {
+      alert('Nope, that\'s wrong...');
+    }
+}
 
 // guess the final answer
 var finalAnswer = prompt('So what\'s my name?');
