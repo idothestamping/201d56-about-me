00bce1b36d90f003e397eef9d703bed1188f12c2
idothestamping <douglas.klemp@gmail.com>
2019-03-27 22:24:22 -0700

    refactor question 6, added question 7, cleaned up white spaces, added additiona comments

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var myQuestions = [
   ['Am I awesome? yes or no'],
   ['Did I make this site in 1hr? yes or no'],
   ['Do I like Trump? yes or no'],
-  ['Was I already an Amazonian? yes or no']
+  ['Was I already an Amazonian? yes or no'],
 ];
 var myAnswers = [
   ['YES'],
@@ -14,9 +14,10 @@ var myAnswers = [
   ['NO'],
   ['YES'],
   [5],
+  ['strawberry', 'chocolate', 'green tea', 'rocky road']
 ];
 var totalCount = 0
-// // personalize user experience with their name
+// personalize user experience with their name
 var userName = prompt('What is your name?');
 if(userName === 'doug'){
   console.log('Hey, we\'ve got the same name!');
@@ -36,39 +37,50 @@ for(var i=0; i<myQuestions.length; i += 1 ) { // loop through questions
   }
 }
 
-// Question Six: Guessing the number with 3 tries
+// Question 6: Guessing the number with 3 tries
 var numberOfChances = 0;
-
 var numberQuestion = prompt('How many dirty martini\'s can I drink in one setting? (enter a number)');
 while (numberQuestion != myAnswers[5] && numberOfChances <= 2 ) {
   if (myAnswers[5] < numberQuestion) {
-    numberQuestion = prompt("You've guessed too high! Keep guessing!");
+    numberQuestion = prompt('You\'ve guessed too high! Keep guessing!');
   } else {
-    numberQuestion = prompt("You've guessed too low! Keep guessing!");
+    numberQuestion = prompt('You\'ve guessed too low! Keep guessing!');
   }
   numberOfChances++;
   console.log(numberOfChances);
-  if (myAnswers[5] === numberQuestion){
-    alert('You\'re Right!');
-  } else if(numberOfChances >= 3){
-    alert('Let\'s move on...');
-  }
 }
 
-totalCount++;
+// Needs more work... keep to ask for feedback for Question 6.
+// if (numberQuestion === MyAnswers[5]){
+//   alert('You\'re Right!');
+//   totalCount++; // Count of questions with correct answer.
+// } else if(numberOfChances >= 2){
+//   alert('Let\'s move on...');
+// }
+
+// Question 7: Multiple Possible Answers
+var numberOfChances2 = 0;
+var iceCreamQuestion = prompt('Name any 1 of 4 ice cream flavors I like?');
+if(myAnswers[6].indexOf(iceCreamQuestion) == -1 && numberOfChances2 <= 2) {
+  prompt('Try another flavor');
+  numberOfChances++;
+} else {
+  totalCount++;
+  alert('Nice, you got it right!');
+}
 
 // guess the final answer
-var finalAnswer = prompt('So what\'s my name?');
+var finalAnswer = prompt('Final Question: So what\'s my name?');
 if(finalAnswer === 'doug'){
   console.log('Congrats, you guessed who I am!!');
   alert('Congrats, you guessed who I am!!');
-  document.getElementById('picbox').classList.add('win');
-  document.getElementById('result').innerHTML = 'Great job mate!!! You\'ve got ' + totalCount + ' answer(s) correct!';
+  document.getElementById('picbox').classList.add('win'); // Add win image to body
+  document.getElementById('result').innerHTML = 'Great job mate!!! You\'ve got ' + totalCount + ' answer(s) correct!'; // Add win statement to body
 } else {
   console.log('That\'s not my name, sorry.');
   alert('That\'s not my name, sorry.');
-  document.getElementById('picbox').classList.add('fail');
-  document.getElementById('result').innerHTML = 'Sorry, that\'s not me, Try again... You\'ve only got' + totalCount + ' answer(s) correct!';
+  document.getElementById('picbox').classList.add('fail'); // Add win image to body
+  document.getElementById('result').innerHTML = 'Sorry, that\'s not me, Try again... You\'ve got ' + totalCount + ' answer(s) correct!'; // Add win statement to body
 }
 
 
